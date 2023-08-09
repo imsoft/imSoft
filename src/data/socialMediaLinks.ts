4177f233d088ed838f2eb97c9b22f3de07553c67
Brandon Garcia <44207909+BUGR2487@users.noreply.github.com>
2023-08-09 00:18:39 -0600

    Se agrego la red social de threads

--- src/data/socialMediaLinks.ts
+++ src/data/socialMediaLinks.ts
@@ -1,6 +1,7 @@
 const facebookLink = "https://www.facebook.com/weareimsoft";
 const instagramLink = "https://www.instagram.com/weareimsoft/";
 const linkedInLink = "https://www.linkedin.com/company/imsoft/";
+const threadsLink = "https://www.threads.net/@weareimsoft";
 const twitterLink = "https://twitter.com/weareimsoft";
 const tiktokLink = "https://www.tiktok.com/@weareimsoft";
 const whatsappBusinessLink = "https://api.whatsapp.com/message/6P7VAOCL442IH1?autoload=1&app_absent=0";
@@ -11,6 +12,7 @@ export {
   facebookLink,
   instagramLink,
   linkedInLink,
+  threadsLink,
   twitterLink,
   tiktokLink,
   whatsappBusinessLink,
